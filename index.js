26f2059f112ce3e925221c440d67d0193976d926
zaatsherif-tech <zaatsherif@gmail.com>
2026-07-04 20:51:23 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running!');
+});
+server.listen(process.env.PORT || 3000);
