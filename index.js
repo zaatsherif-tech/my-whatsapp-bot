2a5726a5c3c713e5ac428967a57db08428fe2173
zaatsherif-tech <zaatsherif@gmail.com>
2026-07-06 19:49:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot is running!');
-});
-server.listen(process.env.PORT || 3000);
+const { makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+const pino = require('pino');
+
+async function startBot() {
+    const { state, saveCreds } = await useMultiFileAuthState('session');
+    const sock = makeWASocket({
+        logger: pino({ level: 'silent' }),
+        auth: state,
+    });
+
+    sock.ev.on('creds.update', saveCreds);
+    sock.ev.on('connection.update', (update) => {
+        const { connection } = update;
+        if (connection === 'open') console.log('البوت اتصل بالواتساب يا دازاي!');
+    });
+}
+startBot();
+
