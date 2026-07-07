c4af176988ea21cf3238c29f4eac8c2a8f5e2a16
zaatsherif-tech <zaatsherif@gmail.com>
2026-07-07 17:07:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState('session');
     const sock = makeWASocket({
         logger: pino({ level: 'silent' }),
+        printQRInTerminal: true, // ده اللي هيخلي الـ QR يظهر في السجلات
         auth: state,
     });
 
     sock.ev.on('creds.update', saveCreds);
     sock.ev.on('connection.update', (update) => {
         const { connection } = update;
-        if (connection === 'open') console.log('البوت اتصل بالواتساب يا دازاي!');
+        if (connection === 'open') console.log('البوت اشتغل يا دازاي!');
     });
 }
 startBot();
